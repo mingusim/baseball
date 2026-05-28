712148d1089b898fdc793cace21f9a8b7e607ce7
mingu.sim <simmg0202@naver.com>
2026-05-28 13:39:21 +0900

    [feature] step2 - 22

--- baseball/baseball_test.cpp
+++ baseball/baseball_test.cpp
@@ -8,3 +8,9 @@ TEST(BaseeballGame, ThrowExceptionWhenLengthIsUnmached) {
 	EXPECT_THROW(game.guess(string("12")), length_error);
 
 }
+
+TEST(BaseeballGame, ThrowExceptionWhenInvalidChar) {
+	Baseball game;
+	EXPECT_THROW(game.guess(string("12s")), invalid_argument);
+
+}

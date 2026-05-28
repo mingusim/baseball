ab1d9d0dd3f8a1af885ef886860fc3682ab2217e
mingu.sim <simmg0202@naver.com>
2026-05-28 13:55:47 +0900

    [refactoring] step 3 - 34

--- baseball/baseball.cpp
+++ baseball/baseball.cpp
@@ -5,19 +5,27 @@ using namespace std;
 class Baseball {
 public:
 	void guess(const string& guessNum) {
+		assertIllegalArgument(guessNum);
+	}
+
+	void assertIllegalArgument(const std::string& guessNum)
+	{
 		if (guessNum.length() != 3) {
 			throw length_error("Must be three letters.");
 		}
 		for (char ch : guessNum) {
-			if (ch < '0' || ch > '9') {
-				throw invalid_argument("Must be number");
-			}
+			if (ch >= '0' && ch <= '9') continue;
+			throw invalid_argument("Must be number");
 		}
-		if(guessNum[0] == guessNum[1]
-			|| guessNum[1] == guessNum[2]
-			|| guessNum[2] == guessNum[0])
+		if (isDuplicateNumber(guessNum))
 		{
 			throw invalid_argument("Must not have the same number");
 		}
+	}
+	bool isDuplicateNumber(const std::string& guessNum)
+	{
+		return guessNum[0] == guessNum[1]
+			|| guessNum[1] == guessNum[2]
+			|| guessNum[2] == guessNum[0];
 	}
 };
